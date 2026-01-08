0809faee83c73655d2f5ed336b87711a2da9f252
alaabadr4410 <Alaabadr4470@gmail.com>
2026-01-09 01:13:06 +0200

    fix: App.tsx syntax imports and closing brace

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -30,7 +31,6 @@ const AdminGroup = () => (
   </ProtectedRoute>
 );
 
-import { useEffect } from 'react';
 
 const queryClient = new QueryClient();
 
@@ -104,6 +104,7 @@ const App = () => {
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
-);
+  );
+};
 
 export default App;
