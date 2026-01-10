b3cda98e7225c1fc9b6cc98ab0c07bacef1237e0
alaabadr4410 <Alaabadr4470@gmail.com>
2026-01-10 13:23:11 +0200

    enableLoginDebug: false,

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,6 @@ export const config = {
   // Toggle this to show/hide debug tools like the logout button and socket event tracker
   enableDebugTools: false,
   // Toggle to show the debug credentials dropdown on the login screen
-  enableLoginDebug: true,
+  enableLoginDebug: false,
 };
 
