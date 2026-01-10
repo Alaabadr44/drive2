6508b20e69fe5625747023794b6f1f50125e5ec9
alaabadr4410 <Alaabadr4470@gmail.com>
2026-01-10 14:07:08 +0200

    Moved Menu Up Further ⬆️

--- frontend/src/pages/RestaurantDetail.tsx
+++ frontend/src/pages/RestaurantDetail.tsx
@@ -162,7 +162,7 @@ export function RestaurantDetail({ restaurant, onBack, onReset }: RestaurantDeta
       />
       
       {/* Centered Menu Viewer Section */}
-      <div className="flex-1 relative flex items-center justify-center w-full h-full px-4 pb-[270px] pt-10 overflow-hidden">
+      <div className="flex-1 relative flex items-center justify-center w-full h-full px-4 pb-[320px] pt-10 overflow-hidden">
         <div className="relative w-full h-full flex items-center justify-center animate-in fade-in zoom-in duration-500">
           
           {/* Navigation Arrows */}
